be4d34f1043b6e4a9398d5cfd5a0f3fa7edd3c66
Mingze-Li0625 <mingze.li0625@gmail.com>
2025-05-27 11:53:22 -0400

    add comments of sample route

--- R/sample_route.R
+++ R/sample_route.R
@@ -3,17 +3,19 @@
 #' This function samples trips based on the distance range. 
 #' If a single distance is provided, it will set the lower and upper bounds to 90% and 110% of the distance,
 #' respectively. If a vector of two distances is provided, it will use the first distance as the lower bound
-#'  and the second distance as the upper bound. The function returns a data.table containing the filtered trips.
+#'  and the second distance as the upper bound. The function returns a data.table containing the sampled 
+#' trips within the distance range.
 #' 
-#' @param distance numeric|numeric vector Distance range.
+#' @param distance numeric/numeric vector. The distance range.
 #'  If a single distance is provided, it will set the lower and upper bounds to 90% and 110% of the distance, respectively.
 #'  If a vector of two distances is provided, it will use the first distance as the lower bound and the second distance as the upper bound.
-#' @param trips data.table Trips data.
-#' @param r numeric Number of samples.
-#' @return data.table Filtered trips.
+#' @param trips data.table. Trips data, with columns trip, time, length.
+#' @param r numeric. Number of trips that wish to sample.
+#' @return a data.table that contains sampled trips within given distance range.
 #' @author Mingze Li <mingzeli7@cmail.carleton.ca>
 #' @examples
 #' sample_route(1000, trips, 10)
+#' @export
 sample_route <- function(distance,trips, r) {
     if(length(distance) == 1) {
        lower_distance <- distance *0.9
@@ -27,5 +29,7 @@ sample_route <- function(distance,trips, r) {
     real_length$time <- as.numeric(trips[, .(difftime(time[.N], time[1], units = "secs")), trip]$V1)
     # Filter trips based on distance range
     filtered_trips <- real_length[distance >= lower_distance & distance <= upper_distance]
-    return(filtered_trips)
+    # Sample trips based on distance range
+    sampled_trips <- filtered_trips[sample(1:nrow(filtered_trips), r, replace = TRUE)]
+    return(sampled_trips)
 }
